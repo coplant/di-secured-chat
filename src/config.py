f5e46507c437e13a7be20ecdac1ff68be0e058f4
Ivan <omg.dayz@gmail.com>
2023-04-10 02:52:23 +0700

    add: tests

--- src/config.py
+++ src/config.py
@@ -15,3 +15,9 @@ DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 
 DEFAULT_CHUNK_SIZE = 1024 * 1024 * 5
+
+DB_HOST_TEST = os.environ.get("DB_HOST_TEST")
+DB_PORT_TEST = os.environ.get("DB_PORT_TEST")
+DB_NAME_TEST = os.environ.get("DB_NAME_TEST")
+DB_USER_TEST = os.environ.get("DB_USER_TEST")
+DB_PASS_TEST = os.environ.get("DB_PASS_TEST")
