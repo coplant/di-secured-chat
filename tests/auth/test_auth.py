b934737d6a1f3293d3eb9039b43bc0b8b7f618fd
Ivan <omg.dayz@gmail.com>
2023-04-10 17:58:03 +0700

    upd: tests

--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -1,11 +1,17 @@
 import base64
+import hashlib
+import json
+from datetime import datetime
 
 import pytest
 import rsa
-from httpx import Response
+from httpx import Response, AsyncClient
+from sqlalchemy import insert, select
 
-from src.config import PUBLIC_KEY, PRIVATE_KEY
-from tests.conftest import client
+from src.auth.models import User, Role
+from src.config import PUBLIC_KEY, PRIVATE_KEY, BASE_DIR
+from src.utils import get_public_key, get_private_key
+from tests.conftest import client, async_session_maker
 
 
 def test_get_public_key():
@@ -18,8 +24,97 @@ def test_get_public_key():
     public_key = base64.b64decode(response.json()['public_key'])
     signature = rsa.sign(public_key, raw_private_key, 'SHA-256')
 
+    assert raw_public_key == get_public_key()
+    assert raw_private_key == get_private_key()
+
     assert response.status_code == 200
     assert rsa.PublicKey.load_pkcs1(public_key) == raw_public_key
     assert base64.b64decode(response.json()['signature']) == raw_signature
     assert rsa.verify(public_key, signature, raw_public_key) == 'SHA-256'
     assert signature == raw_signature
+
+
+async def test_add_roles():
+    async with async_session_maker() as session:
+        roles_data = [
+            {'id': 100, 'name': 'admin'},
+            {'id': 0, 'name': 'user'}
+        ]
+        stmt = insert(Role).values(roles_data)
+        await session.execute(stmt)
+        await session.commit()
+
+        query = select(Role)
+        result = await session.execute(query)
+        items = [{"id": item.id, "name": item.name} for item in result.scalars().all()]
+        assert items == [
+            {'id': 100, 'name': 'admin'},
+            {'id': 0, 'name': 'user'}
+        ], "Roles have not been added"
+
+
+async def test_add_admin():
+    async with async_session_maker() as session:
+        admin_data = {
+            'id': 1,
+            'uid': '41119e27d08d48105c425c1e5102f9626a1b25627eb23da5fce27d791efe3f81',
+            'name': 'Администратор',
+            'username': 'admin',
+            'hashed_password': '$2b$12$8UcA8nUE7nBBp8s6GvkGi.J.Jlt3Wi6UvvsrcUb8tm3gXwv9vwKny',
+            'has_changed_password': True,
+            'logged_at': datetime.utcnow(),
+            'created_at': datetime.utcnow(),
+            'changed_at': datetime.utcnow(),
+            'role_id': 100,
+            'is_active': True
+        }
+        stmt = insert(User).values(**admin_data)
+        await session.execute(stmt)
+        await session.commit()
+
+        query = select(User)
+        result = await session.execute(query)
+        expected_user = User(**admin_data)
+        actual_user = result.scalars().first()
+        assert expected_user.id == actual_user.id
+        assert expected_user.uid == actual_user.uid
+        assert expected_user.name == actual_user.name
+        assert expected_user.username == actual_user.username
+        assert expected_user.hashed_password == actual_user.hashed_password
+        assert expected_user.has_changed_password == actual_user.has_changed_password
+        assert expected_user.logged_at == actual_user.logged_at
+        assert expected_user.created_at == actual_user.created_at
+        assert expected_user.changed_at == actual_user.changed_at
+        assert expected_user.role_id == actual_user.role_id
+        assert expected_user.is_active == actual_user.is_active
+
+
+async def test_login(ac: AsyncClient):
+    # то, что вводит клиент
+    raw_user_data = {
+        "username": "admin",
+        "password": "uZqXYrK3Mu_Fg-7w",
+        "uid": "B272CE72-DA23-4D68-AB4F-26ABFD9735CA",
+    }
+    # то, что отправится
+    encrypted_user_data = {}
+
+    # получаем ключи клиента
+    with open(BASE_DIR / "keys" / "user_private.pem", "rb") as file:
+        user_private_key = rsa.PrivateKey.load_pkcs1(file.read())
+    with open(BASE_DIR / "keys" / "user_public.pem", "rb") as file:
+        user_public_key = rsa.PublicKey.load_pkcs1(file.read())
+
+    # собираем словарь — зашифровываем уязвимые данные, передаем при помощи base64
+    encrypted_user_data["username"] = base64.b64encode(
+        rsa.encrypt(raw_user_data["username"].encode(), get_public_key())).decode()
+    encrypted_user_data["password"] = base64.b64encode(
+        rsa.encrypt(raw_user_data["password"].encode(), get_public_key())).decode()
+    encrypted_user_data["uid"] = base64.b64encode(
+        rsa.encrypt(hashlib.sha256(raw_user_data["uid"].encode()).digest(), get_public_key())).decode()
+    encrypted_user_data["public_key"] = base64.b64encode(
+        user_public_key.save_pkcs1("PEM")).decode()
+    encrypted_user_data["signature"] = base64.b64encode(
+        rsa.sign(json.dumps(encrypted_user_data).encode(), user_private_key, "SHA-256")).decode()
+    response = await ac.post(url='/api/auth/login', json=encrypted_user_data)
+    assert response.status_code == 200
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,16 @@
 import asyncio
+import os
 from typing import AsyncGenerator
 
 import pytest
 from fastapi.testclient import TestClient
-from httpx import AsyncClient
+from httpcore import HTTPProxy
+from httpx import AsyncClient, Proxy
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.pool import NullPool
 
 from src.database import get_async_session
-from src.database import Base as metadata
+from src.database import Base
 from src.config import (DB_HOST_TEST, DB_NAME_TEST, DB_PASS_TEST, DB_PORT_TEST,
                         DB_USER_TEST)
 from src.main import app
@@ -18,7 +20,7 @@ DATABASE_URL_TEST = f"postgresql+asyncpg://{DB_USER_TEST}:{DB_PASS_TEST}@{DB_HOS
 
 engine_test = create_async_engine(DATABASE_URL_TEST, poolclass=NullPool)
 async_session_maker = async_sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False)
-metadata.bind = engine_test
+Base.metadata.bind = engine_test
 
 
 async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
@@ -32,10 +34,10 @@ app.dependency_overrides[get_async_session] = override_get_async_session
 @pytest.fixture(autouse=True, scope='session')
 async def prepare_database():
     async with engine_test.begin() as conn:
-        await conn.run_sync(metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
     yield
     async with engine_test.begin() as conn:
-        await conn.run_sync(metadata.drop_all)
+        await conn.run_sync(Base.metadata.drop_all)
 
 
 # SETUP
@@ -48,9 +50,10 @@ def event_loop(request):
 
 
 client = TestClient(app)
+os.environ['HTTP_PROXY'] = 'http://127.0.0.1:8888'
 
 
 @pytest.fixture(scope="session")
 async def ac() -> AsyncGenerator[AsyncClient, None]:
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000", proxies=Proxy(url='http://127.0.0.1:8888')) as ac:
         yield ac
