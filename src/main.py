5005ba06e13cb88cb5ee0fde03c2e17e77aee964
Ivan <omg.dayz@gmail.com>
2023-04-13 01:06:59 +0700

    add: test chat router

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from pydantic import ValidationError
 
 from src.auth.router import router as auth_router
 from src.chat.router import router as chat_router
-from src.chat.test_r import router as octet_router
+# from src.chat.test_r import router as octet_router
 from src.exceptions import http_exception_handler, validation_exception_handler
 from src.utils import RSA
 
@@ -22,7 +22,7 @@ route = APIRouter(prefix="/api", tags=[])
 
 route.include_router(auth_router)
 route.include_router(chat_router)
-route.include_router(octet_router)
+# route.include_router(octet_router)
 app.include_router(route)
 
 if __name__ == "__main__":
