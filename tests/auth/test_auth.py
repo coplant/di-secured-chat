62aa96a6e7cb1f9d67b8d0ac255e28e4f95e15db
Ivan <omg.dayz@gmail.com>
2023-04-13 22:16:13 +0700

    upd: change-password test

--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -2,16 +2,14 @@ import base64
 import hashlib
 import json
 from datetime import datetime
-
 import bcrypt
-import pytest
 import rsa
 from httpx import Response, AsyncClient
 from sqlalchemy import insert, select
 
 from src.auth.models import User, Role
-from src.config import PUBLIC_KEY, PRIVATE_KEY, BASE_DIR, HASH_TYPE
-from src.utils import RSA, get_current_user
+from src.config import PUBLIC_KEY, PRIVATE_KEY, HASH_TYPE
+from src.utils import RSA
 from tests.auth.utils import get_token_from_client
 from tests.conftest import client, async_session_maker
 
@@ -189,34 +187,39 @@ async def test_valid_logout(ac: AsyncClient, get_keys, data=None):
     assert decrypted["data"]["details"] == "logged out"
 
 
-async def test_change_password(ac: AsyncClient, get_keys):
+async def test_change_password(ac: AsyncClient, get_keys, data=None):
     server_private_key, server_public_key, user_private_key, user_public_key = get_keys
-    raw_user_data = {
-        "password": "MyNewPassword",
-    }
-    # то, что отправится после обработки
-    encrypted_user_data = {
-        "password": base64.b64encode(
-            rsa.encrypt(raw_user_data["password"].encode(), server_public_key)).decode()
-    }
-
-    # собираем словарь — зашифровываем уязвимые данные, передаем при помощи base64
-    token = await test_login(ac, get_keys)
-    token = get_token_from_client(token, get_keys)
-    response = await ac.patch(url=f'/api/auth/change-password?token={token}', json=encrypted_user_data)
+    if data is None:
+        data = {
+            "payload": {
+                "password": "MyNewPassword"
+            },
+            "token": await test_login(ac, get_keys)
+        }
+    signature = base64.b64encode(rsa.sign(json.dumps(data).encode(), user_private_key, HASH_TYPE)).decode()
+    to_send = {"data": data, "signature": signature}
+    to_send = json.dumps(to_send)
+    encrypted_msg = RSA.encrypt(to_send.encode(), server_public_key)
+    response = await ac.patch(url=f'/api/auth/change-password',
+                              content=encrypted_msg,
+                              headers={"Content-Type": "application/octet-stream"})
     assert response.status_code == 200
-    assert response.json()['status'] == 'success'
-    assert response.json()['details']
+    decrypted = json.loads(RSA.decrypt(response.content, user_private_key))
+    is_valid = check_signature(decrypted, server_public_key)
+    assert is_valid is True
+    assert decrypted["data"]["status"] == "success"
+    assert decrypted["data"]["details"]
 
-    token = await test_login(ac, get_keys, raw_user_data={
-        "username": "admin",
-        "password": "MyNewPassword",
-        "uid": "B272CE72-DA23-4D68-AB4F-26ABFD9735CA",
+    token: str = await test_login(ac, get_keys, data={
+        "payload": {
+            "username": "admin",
+            "password": "MyNewPassword",
+            "uid": "CCE94688-C176-4D12-8115-A96CEC9B809F",
+            "public_key": base64.b64encode(user_public_key.save_pkcs1(format="DER")).decode(),
+        }
     })
-    token = get_token_from_client(token, get_keys)
-    token = rsa.decrypt(base64.urlsafe_b64decode(token), RSA.get_private_key())
-    query = select(User).filter_by(hashed_token=hashlib.sha256(token).hexdigest())
+    query = select(User).filter_by(hashed_token=hashlib.sha256(token.encode()).hexdigest())
     async with async_session_maker() as session:
         result = await session.execute(query)
     user = result.scalars().unique().first()
-    assert bcrypt.checkpw(raw_user_data["password"].encode(), user.hashed_password.encode()) is True
+    assert bcrypt.checkpw(data["payload"]["password"].encode(), user.hashed_password.encode()) is True
