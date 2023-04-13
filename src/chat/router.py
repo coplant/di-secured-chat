d82a3697bc50560383cbfd7767e53b42bc2c413a
Ivan <omg.dayz@gmail.com>
2023-04-13 23:49:58 +0700

    add: get active users

--- src/chat/router.py
+++ src/chat/router.py
@@ -5,34 +5,47 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
-from starlette.responses import JSONResponse
+from starlette.responses import Response
 
 from src.auth.models import User
+from src.auth.schemas import RequestSchema
 from src.chat.schemas import GetUsersSchema, GetUserSchema
 from src.database import get_async_session
-from src.schemas import ValidationResponseSchema
-from src.utils import get_current_user
+from src.utils import get_user_by_token, prepare_encrypted, RSA
 
 router = APIRouter(tags=["Chat"], prefix="/chat")
 
 
-@router.get("/get-users", response_model=GetUsersSchema, responses={422: {"model": ValidationResponseSchema}})
-async def get_users(user: User = Depends(get_current_user),
+@router.post("/get-users", responses={422: {"model": ""}})
+async def get_users(encrypted: tuple[RequestSchema, User] = Depends(get_user_by_token),
+                    server_private_key: rsa.PrivateKey = Depends(RSA.get_private_key),
                     session: AsyncSession = Depends(get_async_session)):
+    decrypted, user = encrypted
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="unauthorized")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    user_public_key = rsa.PublicKey.load_pkcs1(base64.b64decode(user.public_key), "DER")
     if not user.has_changed_password:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="password expired")
+        data = {
+            "status": "error",
+            "data": None,
+            "details": "password expired"
+        }
+        encrypted = prepare_encrypted(data, server_private_key, user_public_key)
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=encrypted)
     try:
         query = select(User).filter_by(is_active=True)
         result = await session.execute(query)
         result = result.scalars().all()
         user_public_key = rsa.PublicKey.load_pkcs1(base64.b64decode(user.public_key), "DER")
-        data = [GetUserSchema(id=base64.b64encode(rsa.encrypt(str(item.id).encode(), user_public_key)).decode(),
-                              name=base64.b64encode(rsa.encrypt(item.name.encode(), user_public_key)).decode(),
-                              username=base64.b64encode(rsa.encrypt(item.username.encode(), user_public_key)).decode()
-                              ).dict() for item in result]
-        return JSONResponse(status_code=status.HTTP_200_OK,
-                            content={"status": "success", "data": data, "details": ""})
+        data = [GetUserSchema(id=item.id, name=item.name, username=item.username).dict() for item in result]
+        data = {
+            "status": "success",
+            "data": data,
+            "details": None
+        }
+        encrypted = prepare_encrypted(data, server_private_key, user_public_key)
+        response = Response(status_code=status.HTTP_200_OK, content=encrypted,
+                            media_type="application/octet-stream")
+        return response
     except Exception:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="unauthorized")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
--- src/chat/schemas.py
+++ src/chat/schemas.py
@@ -1,13 +1,28 @@
+from typing import Union
+
 from pydantic import Field, BaseModel
 
 from src.schemas import ResponseSchema
 
 
 class GetUserSchema(BaseModel):
-    id: str = Field(..., example="base64 encoded id")
-    username: str = Field(..., example="base64 encoded username")
-    name: str = Field(..., example="base64 encoded name")
+    id: str
+    username: str
+    name: str
 
 
 class GetUsersSchema(ResponseSchema):
     data: list[GetUserSchema]
+
+
+class PayloadSchema(BaseModel):
+    payload: Union[dict]
+
+
+class PayloadTokenSchema(PayloadSchema):
+    token: str
+
+
+class RequestSchema(BaseModel):
+    data: Union[PayloadTokenSchema, PayloadSchema]
+    signature: str
